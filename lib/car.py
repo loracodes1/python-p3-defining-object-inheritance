4d46aecb81fde705f5bb930f5cf019a591dc9529
florence <loracodes3@gmail.com>
2024-12-07 20:34:49 +0300

    inheritance

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,10 @@
 from vehicle import Vehicle
 
+from vehicle import Vehicle
+
 class Car(Vehicle):
-    pass
+    def go(self):
+        """
+        Overrides the go method of the Vehicle class to customize it for Car.
+        """
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
--- lib/vehicle.py
+++ lib/vehicle.py
@@ -1,2 +1,21 @@
 class Vehicle:
-    pass
+    def __init__(self, wheel_size, wheel_number):
+        """
+        Initializes a Vehicle instance with wheel size and number.
+        """
+        self.wheel_size = wheel_size
+        self.wheel_number = wheel_number
+
+    def go(self):
+        """
+        Simulates the vehicle moving.
+        """
+        return "vrrrrrrrooom!"
+
+    def fill_up_tank(self):
+        """
+        Simulates filling up the vehicle's tank.
+        """
+        return "filling up!"
+
+
